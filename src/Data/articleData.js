72dfacbb60fd80df9c65352a45dac10b57e4682c
Kang Kujun <donato14@naver.com>
2022-04-09 01:25:37 +0900

    🍱 Add articleData

---
+++ src/Data/articleData.js
@@ -0,0 +1,94 @@
+let articleData = [
+  {
+    id : 0,
+    title : "런던에서 즐기는 크리스마스는 어떨까?",
+    content: "",
+    autor: "규준",
+  },
+
+  {
+    id : 1,
+    title : "새벽의 에펠탑",
+    content: "",
+    autor: "규준",
+  },
+
+  {
+    id : 2,
+    title : "빨간 버스, Corinthia hotel",
+    content: "",
+    autor: "규준",
+  },
+
+  {
+    id : 3,
+    title : "제주도에서의 차박",
+    content: "",
+    autor: "규준",
+  },
+
+  {
+    id : 4,
+    title : "프라하 구시가지의 크리스마스 마켓",
+    content: "",
+    autor: "규준",
+  },
+
+  {
+    id : 5,
+    title : "일상 속, 여행자의 시선",
+    content: "",
+    autor: "규준",
+  },
+
+  {
+    id : 6,
+    title : "트레비 분수는 나의 소원을 이뤄줄까?",
+    content: "",
+    autor: "규준",
+  },
+
+  {
+    id : 7,
+    title : "시애틀 다운타운 방향의 야경",
+    content: "",
+    autor: "규준",
+  },
+
+  {
+    id : 8,
+    title : "프라하 성 스타벅스에서 먹는 와플",
+    content: "",
+    autor: "규준",
+  },
+
+  {
+    id : 9,
+    title : "파리의 화창한 날",
+    content : "",
+    autor : "규준",
+  },
+
+  {
+    id : 10,
+    title : "용머리 해안 포토 스팟",
+    content: "",
+    autor: "규준",
+  },
+
+  {
+    id : 11,
+    title : "노을 맛집 함덕 해변",
+    content: "",
+    autor: "규준",
+  },
+
+  {
+    id : 12,
+    title : "용머리 해안에 가면 꼭 사진 찍으세요!",
+    content: "",
+    autor: "규준",
+  }
+] 
+
+export default articleData;
