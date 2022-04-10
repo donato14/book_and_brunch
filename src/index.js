332987301681d2f6996966a6aa664e4784907829
Kang Kujun <donato14@naver.com>
2022-04-10 17:56:57 +0900

    🚧 Add articleData using Redux

--- src/index.js
+++ src/index.js
@@ -2,15 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
+import articleData from './Data/articleData';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+// function articleData(state = articleData, action) {
+//   return state
+// }
+
+function reducer(state = articleData, action) {
+  return state
+}
+
+let store = createStore(reducer);
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      {/* <Provider> */}
+      <Provider store={store}>
         <App />
-      {/* </Provider> */}
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
