690d3b69ed9afea08fd662b96dbcd5963b526ed0
Kang Kujun <donato14@naver.com>
2022-04-13 23:58:05 +0900

    🚧 Add cardData

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import keywordData from './Data/keywordData';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { combineReducers, createStore } from 'redux';
+import cardData from './Data/cardData';
 
 // function articleData(state = articleData, action) {
 //   return state
@@ -20,7 +21,11 @@ function reducer2(state = keywordData, action) {
   return state
 }
 
-let store = createStore(combineReducers({reducer, reducer2}));
+function reducer3(state = cardData, action) {
+  return state
+}
+
+let store = createStore(combineReducers({reducer, reducer2, reducer3}));
 
 ReactDOM.render(
   <React.StrictMode>
